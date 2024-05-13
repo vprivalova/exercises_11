d59451889686475c6799aa218683d9a8737b2e35
vprivalova <privalovaa.vv@gmail.com>
2024-05-13 16:40:12 +0700

    task 2 update

--- task_02.py
+++ task_02.py
@@ -1,3 +1,6 @@
+import ean_decoder
+
+
 class ShoppingChart:
     all_goods = {}
 
@@ -6,7 +9,7 @@ class ShoppingChart:
 
     @property
     def current(self):
-        return '\n'.join(self.__current)
+        return self.__current
 
     @current.setter
     def current(self, new):
@@ -22,7 +25,7 @@ class ShoppingChart:
         with open(file, 'r', encoding='utf8') as f:
             for line in f:
                 name, code = line.strip().split(';')
-                result = Product(name, code)
+                result = Product(name, code.strip())
                 cls.all_goods[result.__repr__()[0]] = result.__repr__()[1:]
 
     def add_product(self, product):
@@ -31,12 +34,24 @@ class ShoppingChart:
     def delete_product(self, product):
         self.current = ('del' + product)
 
+    def show_chart(self):
+        for elem in self.__current:
+            print(elem, *ShoppingChart.all_goods[elem])
+
+    @classmethod
+    def assortment(cls):
+        for key in cls.all_goods:
+            print(key, *cls.all_goods[key])
+
 
 class Product:
     def __init__(self, name, code):
         self.__name = name
         self.__code = code
-        self.country = 'russia'
+        if code[0:3] in ean_decoder.ean:
+            self.country = ean_decoder.ean[code[0:3]]
+        else:
+            self.country = ''
 
     @property
     def name(self):
@@ -57,9 +72,10 @@ class Menu:
         chart = ShoppingChart()
         intro = (f'\n'
                  f'Для загрузки данных о товарах - введите 1\n'
-                 f'Для добавления товара в вашу корзину - введите 2\n'
-                 f'Для удаления товара из корзины - введите 3\n'
-                 f'Для просмотра содержания корзины - введите 4\n'
+                 f'Для отображения теущего ассортимента товаров - введите 2\n'
+                 f'Для добавления товара в вашу корзину - введите 3\n'
+                 f'Для удаления товара из корзины - введите 4\n'
+                 f'Для просмотра содержания корзины - введите 5\n'
                  f'Чтобы покинуть интернет-магазин - введите end\n')
 
         print(greeting, intro)
@@ -75,6 +91,12 @@ class Menu:
                 inquiry = input()
 
             elif inquiry == '2':
+                chart.assortment()
+
+                print(intro)
+                inquiry = input()
+
+            elif inquiry == '3':
                 print('Введите название продукта, который нужно добавить в корзину')
                 product = input()
                 chart.add_product(product)
@@ -82,7 +104,7 @@ class Menu:
                 print(intro)
                 inquiry = input()
 
-            elif inquiry == '3':
+            elif inquiry == '4':
                 print('Введите название продукта, который нужно удалить из корзину')
                 product = input()
                 chart.delete_product(product)
@@ -90,8 +112,8 @@ class Menu:
                 print(intro)
                 inquiry = input()
 
-            elif inquiry == '4':
-                print(chart.current)
+            elif inquiry == '5':
+                chart.show_chart()
 
                 print(intro)
                 inquiry = input()
